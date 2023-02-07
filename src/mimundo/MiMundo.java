eb223b882011207cb67ae86cf5d59823bde4b904
jdiaz <jdiaz@LAPTOP-59K4GJ6D>
2023-02-07 11:00:35 +0100

    Hola Mundo

---
+++ src/mimundo/MiMundo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mimundo;
+
+/**
+ *
+ * @author jdiaz
+ */
+public class MiMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        
+        System.out.println("GIT: Bienvenido");
+
+    }
+    
+}
